9a59707dc81c130b79849b3d0e8991cfdc0f5f1b
gguiulfo <gguiulfo@student.42.us.org>
2017-05-31 02:20:05 -0700

    Norm fix

--- src/ft_term/ft_get_win_size.c
+++ src/ft_term/ft_get_win_size.c
@@ -6,7 +6,7 @@
 /*   By: gguiulfo <gguiulfo@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/29 17:30:06 by gguiulfo          #+#    #+#             */
-/*   Updated: 2017/05/29 18:15:29 by gguiulfo         ###   ########.fr       */
+/*   Updated: 2017/05/31 02:00:28 by gguiulfo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 void	ft_get_win_size(int *x, int *y)
 {
-	struct	winsize win;
+	struct winsize win;
 
 	ioctl(STDIN_FILENO, TIOCGWINSZ, &win);
 	*x = win.ws_col;
