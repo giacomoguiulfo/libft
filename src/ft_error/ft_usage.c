de56e64e75669aa02a68e74514f72610c2f1670b
Giacomo Guiulfo <giacomoguiulfo@gmail.com>
2017-10-02 21:27:53 -0700

    Update ft_usage

--- src/ft_error/ft_usage.c
+++ src/ft_error/ft_usage.c
@@ -6,7 +6,7 @@
 /*   By: gguiulfo <gguiulfo@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/01 20:19:08 by gguiulfo          #+#    #+#             */
-/*   Updated: 2017/10/01 20:26:24 by gguiulfo         ###   ########.fr       */
+/*   Updated: 2017/10/02 21:27:22 by gguiulfo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 void	ft_usage(char const *name, char const *str)
 {
-	ft_dprintf(STDERR_FILENO, "%{bred}usage: %s%s%{eoc}", name, str);
+	ft_dprintf(STDERR_FILENO, "%{bred}usage: %s%s%{eoc}\n", name, str);
 	exit(EXIT_FAILURE);
 }
