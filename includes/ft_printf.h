c51560164c3c954d50d388ea1188678ba1037b21
Giacomo Guiulfo <gguiulfo@student.42.us.org>
2017-11-10 22:07:13 -0800

    Add ft_vprintf

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -23,6 +23,7 @@
 int				ft_printf(const char *format, ...);
 int				ft_sprintf(char *str, const char *format, ...);
 int				ft_snprintf(char *str, size_t size, const char *format, ...);
+int				ft_vdprintf(int fd, const char *format, va_list ap);
 int				ft_dprintf(int fd, const char *format, ...);
 int				ft_vasprintf(char **ret, const char *format, va_list ap);
 int				ft_asprintf(char **ret, const char *format, ...);
--- src/ft_printf/ft_printf_more.c
+++ src/ft_printf/ft_printf_more.c
@@ -26,3 +26,16 @@ int		ft_asprintf(char **ret, const char *format, ...)
 	va_end(ap);
 	return (len);
 }
+
+int		ft_vdprintf(int fd, const char *format, va_list ap)
+{
+	int		len;
+	char	*ret;
+
+	if (!format || !*format || fd < 0)
+		return (0);
+	len = ft_vasprintf(&ret, format, ap);
+	write(fd, ret, len);
+	free(ret);
+	return (len);
+}
